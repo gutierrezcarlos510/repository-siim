e16f273973d08de204e45907858d4437b4fa0acf
gutierrezcarlos510 <gutierrezcarlos510@gmail.com>
2022-05-15 23:55:43 -0400

    adicion de modelo de vistas

---
+++ src/main/java/net/siim/manager/model/ViewInmueble.java
@@ -0,0 +1,423 @@
+package net.siim.manager.model;
+
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ViewInmueble {
+
+  private String id;
+  private String idInmu;
+  private BigDecimal tpInmu;
+  private String censal;
+  private String zona;
+  private String barrio;
+  private BigDecimal tipocalle;
+  private String nombrecall;
+  private String numcasa;
+  private String bloque;
+  private String piso;
+  private String numdpto;
+  private String catastral;
+  private String descrip;
+  private BigDecimal matVias;
+  private BigDecimal inclinac;
+  private BigDecimal luz;
+  private BigDecimal agua;
+  private BigDecimal alcantari;
+  private BigDecimal telefono;
+  private BigDecimal superficie;
+  private BigDecimal supConst;
+  private BigDecimal antConst;
+  private BigDecimal calidad;
+  private BigDecimal deuda;
+  private BigDecimal exento;
+  private String resolucion;
+  private BigDecimal revalor;
+  private BigDecimal supHa;
+  private BigDecimal valorHa;
+  private String usuario;
+  private BigDecimal estado;
+  private Boolean imchk;
+  private String ddrr;
+  private String empNescri;
+  private Date empFescri;
+  private String empNpoder;
+  private Date empFpoder;
+  private BigDecimal porc;
+  private BigDecimal codluga;
+  private String xzona;
+  private String xbarrio;
+  private String xtipoCalle;
+  private String xtipoInmueble;
+  private String xmaterialvia;
+  private String xinclinacion;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getIdInmu() {
+    return idInmu;
+  }
+
+  public void setIdInmu(String idInmu) {
+    this.idInmu = idInmu;
+  }
+
+  public BigDecimal getTpInmu() {
+    return tpInmu;
+  }
+
+  public void setTpInmu(BigDecimal tpInmu) {
+    this.tpInmu = tpInmu;
+  }
+
+  public String getCensal() {
+    return censal;
+  }
+
+  public void setCensal(String censal) {
+    this.censal = censal;
+  }
+
+  public String getZona() {
+    return zona;
+  }
+
+  public void setZona(String zona) {
+    this.zona = zona;
+  }
+
+  public String getBarrio() {
+    return barrio;
+  }
+
+  public void setBarrio(String barrio) {
+    this.barrio = barrio;
+  }
+
+  public BigDecimal getTipocalle() {
+    return tipocalle;
+  }
+
+  public void setTipocalle(BigDecimal tipocalle) {
+    this.tipocalle = tipocalle;
+  }
+
+  public String getNombrecall() {
+    return nombrecall;
+  }
+
+  public void setNombrecall(String nombrecall) {
+    this.nombrecall = nombrecall;
+  }
+
+  public String getNumcasa() {
+    return numcasa;
+  }
+
+  public void setNumcasa(String numcasa) {
+    this.numcasa = numcasa;
+  }
+
+  public String getBloque() {
+    return bloque;
+  }
+
+  public void setBloque(String bloque) {
+    this.bloque = bloque;
+  }
+
+  public String getPiso() {
+    return piso;
+  }
+
+  public void setPiso(String piso) {
+    this.piso = piso;
+  }
+
+  public String getNumdpto() {
+    return numdpto;
+  }
+
+  public void setNumdpto(String numdpto) {
+    this.numdpto = numdpto;
+  }
+
+  public String getCatastral() {
+    return catastral;
+  }
+
+  public void setCatastral(String catastral) {
+    this.catastral = catastral;
+  }
+
+  public String getDescrip() {
+    return descrip;
+  }
+
+  public void setDescrip(String descrip) {
+    this.descrip = descrip;
+  }
+
+  public BigDecimal getMatVias() {
+    return matVias;
+  }
+
+  public void setMatVias(BigDecimal matVias) {
+    this.matVias = matVias;
+  }
+
+  public BigDecimal getInclinac() {
+    return inclinac;
+  }
+
+  public void setInclinac(BigDecimal inclinac) {
+    this.inclinac = inclinac;
+  }
+
+  public BigDecimal getLuz() {
+    return luz;
+  }
+
+  public void setLuz(BigDecimal luz) {
+    this.luz = luz;
+  }
+
+  public BigDecimal getAgua() {
+    return agua;
+  }
+
+  public void setAgua(BigDecimal agua) {
+    this.agua = agua;
+  }
+
+  public BigDecimal getAlcantari() {
+    return alcantari;
+  }
+
+  public void setAlcantari(BigDecimal alcantari) {
+    this.alcantari = alcantari;
+  }
+
+  public BigDecimal getTelefono() {
+    return telefono;
+  }
+
+  public void setTelefono(BigDecimal telefono) {
+    this.telefono = telefono;
+  }
+
+  public BigDecimal getSuperficie() {
+    return superficie;
+  }
+
+  public void setSuperficie(BigDecimal superficie) {
+    this.superficie = superficie;
+  }
+
+  public BigDecimal getSupConst() {
+    return supConst;
+  }
+
+  public void setSupConst(BigDecimal supConst) {
+    this.supConst = supConst;
+  }
+
+  public BigDecimal getAntConst() {
+    return antConst;
+  }
+
+  public void setAntConst(BigDecimal antConst) {
+    this.antConst = antConst;
+  }
+
+  public BigDecimal getCalidad() {
+    return calidad;
+  }
+
+  public void setCalidad(BigDecimal calidad) {
+    this.calidad = calidad;
+  }
+
+  public BigDecimal getDeuda() {
+    return deuda;
+  }
+
+  public void setDeuda(BigDecimal deuda) {
+    this.deuda = deuda;
+  }
+
+  public BigDecimal getExento() {
+    return exento;
+  }
+
+  public void setExento(BigDecimal exento) {
+    this.exento = exento;
+  }
+
+  public String getResolucion() {
+    return resolucion;
+  }
+
+  public void setResolucion(String resolucion) {
+    this.resolucion = resolucion;
+  }
+
+  public BigDecimal getRevalor() {
+    return revalor;
+  }
+
+  public void setRevalor(BigDecimal revalor) {
+    this.revalor = revalor;
+  }
+
+  public BigDecimal getSupHa() {
+    return supHa;
+  }
+
+  public void setSupHa(BigDecimal supHa) {
+    this.supHa = supHa;
+  }
+
+  public BigDecimal getValorHa() {
+    return valorHa;
+  }
+
+  public void setValorHa(BigDecimal valorHa) {
+    this.valorHa = valorHa;
+  }
+
+  public String getUsuario() {
+    return usuario;
+  }
+
+  public void setUsuario(String usuario) {
+    this.usuario = usuario;
+  }
+
+  public BigDecimal getEstado() {
+    return estado;
+  }
+
+  public void setEstado(BigDecimal estado) {
+    this.estado = estado;
+  }
+
+  public Boolean getImchk() {
+    return imchk;
+  }
+
+  public void setImchk(Boolean imchk) {
+    this.imchk = imchk;
+  }
+
+  public String getDdrr() {
+    return ddrr;
+  }
+
+  public void setDdrr(String ddrr) {
+    this.ddrr = ddrr;
+  }
+
+  public String getEmpNescri() {
+    return empNescri;
+  }
+
+  public void setEmpNescri(String empNescri) {
+    this.empNescri = empNescri;
+  }
+
+  public Date getEmpFescri() {
+    return empFescri;
+  }
+
+  public void setEmpFescri(Date empFescri) {
+    this.empFescri = empFescri;
+  }
+
+  public String getEmpNpoder() {
+    return empNpoder;
+  }
+
+  public void setEmpNpoder(String empNpoder) {
+    this.empNpoder = empNpoder;
+  }
+
+  public Date getEmpFpoder() {
+    return empFpoder;
+  }
+
+  public void setEmpFpoder(Date empFpoder) {
+    this.empFpoder = empFpoder;
+  }
+
+  public BigDecimal getPorc() {
+    return porc;
+  }
+
+  public void setPorc(BigDecimal porc) {
+    this.porc = porc;
+  }
+
+  public BigDecimal getCodluga() {
+    return codluga;
+  }
+
+  public void setCodluga(BigDecimal codluga) {
+    this.codluga = codluga;
+  }
+
+  public String getXzona() {
+    return xzona;
+  }
+
+  public void setXzona(String xzona) {
+    this.xzona = xzona;
+  }
+
+  public String getXbarrio() {
+    return xbarrio;
+  }
+
+  public void setXbarrio(String xbarrio) {
+    this.xbarrio = xbarrio;
+  }
+
+  public String getXtipoCalle() {
+    return xtipoCalle;
+  }
+
+  public void setXtipoCalle(String xtipoCalle) {
+    this.xtipoCalle = xtipoCalle;
+  }
+
+  public String getXtipoInmueble() {
+    return xtipoInmueble;
+  }
+
+  public void setXtipoInmueble(String xtipoInmueble) {
+    this.xtipoInmueble = xtipoInmueble;
+  }
+
+  public String getXmaterialvia() {
+    return xmaterialvia;
+  }
+
+  public void setXmaterialvia(String xmaterialvia) {
+    this.xmaterialvia = xmaterialvia;
+  }
+
+  public String getXinclinacion() {
+    return xinclinacion;
+  }
+
+  public void setXinclinacion(String xinclinacion) {
+    this.xinclinacion = xinclinacion;
+  }
+}
---
+++ src/main/java/net/siim/manager/model/ViewPersona.java
@@ -0,0 +1,323 @@
+package net.siim.manager.model;
+
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ViewPersona {
+
+  private String id;
+  private BigDecimal persona;
+  private String documento;
+  private BigDecimal tipodocum;
+  private String expedido;
+  private String paterno;
+  private String materno;
+  private String nombre;
+  private String ruc;
+  private String razonSoc;
+  private String zona;
+  private String barrio;
+  private String tipocalle;
+  private String nombrecall;
+  private String numcasa;
+  private String medidor;
+  private String bloque;
+  private String piso;
+  private String numdpto;
+  private String casilla;
+  private String telefono;
+  private String descrip;
+  private String codHam;
+  private String usuario;
+  private String estado;
+  private Date fechaNac;
+  private String imp;
+  private String idnew;
+  private BigDecimal gam;
+  private Date fecReg;
+  private String xtipoDocumento;
+  private String xtipoPersona;
+  private String xzona,xbarrio;
+  private String xtipoCalle;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public BigDecimal getPersona() {
+    return persona;
+  }
+
+  public void setPersona(BigDecimal persona) {
+    this.persona = persona;
+  }
+
+  public String getDocumento() {
+    return documento;
+  }
+
+  public void setDocumento(String documento) {
+    this.documento = documento;
+  }
+
+  public BigDecimal getTipodocum() {
+    return tipodocum;
+  }
+
+  public void setTipodocum(BigDecimal tipodocum) {
+    this.tipodocum = tipodocum;
+  }
+
+  public String getExpedido() {
+    return expedido;
+  }
+
+  public void setExpedido(String expedido) {
+    this.expedido = expedido;
+  }
+
+  public String getPaterno() {
+    return paterno;
+  }
+
+  public void setPaterno(String paterno) {
+    this.paterno = paterno;
+  }
+
+  public String getMaterno() {
+    return materno;
+  }
+
+  public void setMaterno(String materno) {
+    this.materno = materno;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getRuc() {
+    return ruc;
+  }
+
+  public void setRuc(String ruc) {
+    this.ruc = ruc;
+  }
+
+  public String getRazonSoc() {
+    return razonSoc;
+  }
+
+  public void setRazonSoc(String razonSoc) {
+    this.razonSoc = razonSoc;
+  }
+
+  public String getZona() {
+    return zona;
+  }
+
+  public void setZona(String zona) {
+    this.zona = zona;
+  }
+
+  public String getBarrio() {
+    return barrio;
+  }
+
+  public void setBarrio(String barrio) {
+    this.barrio = barrio;
+  }
+
+  public String getTipocalle() {
+    return tipocalle;
+  }
+
+  public void setTipocalle(String tipocalle) {
+    this.tipocalle = tipocalle;
+  }
+
+  public String getNombrecall() {
+    return nombrecall;
+  }
+
+  public void setNombrecall(String nombrecall) {
+    this.nombrecall = nombrecall;
+  }
+
+  public String getNumcasa() {
+    return numcasa;
+  }
+
+  public void setNumcasa(String numcasa) {
+    this.numcasa = numcasa;
+  }
+
+  public String getMedidor() {
+    return medidor;
+  }
+
+  public void setMedidor(String medidor) {
+    this.medidor = medidor;
+  }
+
+  public String getBloque() {
+    return bloque;
+  }
+
+  public void setBloque(String bloque) {
+    this.bloque = bloque;
+  }
+
+  public String getPiso() {
+    return piso;
+  }
+
+  public void setPiso(String piso) {
+    this.piso = piso;
+  }
+
+  public String getNumdpto() {
+    return numdpto;
+  }
+
+  public void setNumdpto(String numdpto) {
+    this.numdpto = numdpto;
+  }
+
+  public String getCasilla() {
+    return casilla;
+  }
+
+  public void setCasilla(String casilla) {
+    this.casilla = casilla;
+  }
+
+  public String getTelefono() {
+    return telefono;
+  }
+
+  public void setTelefono(String telefono) {
+    this.telefono = telefono;
+  }
+
+  public String getDescrip() {
+    return descrip;
+  }
+
+  public void setDescrip(String descrip) {
+    this.descrip = descrip;
+  }
+
+  public String getCodHam() {
+    return codHam;
+  }
+
+  public void setCodHam(String codHam) {
+    this.codHam = codHam;
+  }
+
+  public String getUsuario() {
+    return usuario;
+  }
+
+  public void setUsuario(String usuario) {
+    this.usuario = usuario;
+  }
+
+  public String getEstado() {
+    return estado;
+  }
+
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
+
+  public Date getFechaNac() {
+    return fechaNac;
+  }
+
+  public void setFechaNac(Date fechaNac) {
+    this.fechaNac = fechaNac;
+  }
+
+  public String getImp() {
+    return imp;
+  }
+
+  public void setImp(String imp) {
+    this.imp = imp;
+  }
+
+  public String getIdnew() {
+    return idnew;
+  }
+
+  public void setIdnew(String idnew) {
+    this.idnew = idnew;
+  }
+
+  public BigDecimal getGam() {
+    return gam;
+  }
+
+  public void setGam(BigDecimal gam) {
+    this.gam = gam;
+  }
+
+  public Date getFecReg() {
+    return fecReg;
+  }
+
+  public void setFecReg(Date fecReg) {
+    this.fecReg = fecReg;
+  }
+
+  public String getXtipoDocumento() {
+    return xtipoDocumento;
+  }
+
+  public void setXtipoDocumento(String xtipoDocumento) {
+    this.xtipoDocumento = xtipoDocumento;
+  }
+
+  public String getXtipoPersona() {
+    return xtipoPersona;
+  }
+
+  public void setXtipoPersona(String xtipoPersona) {
+    this.xtipoPersona = xtipoPersona;
+  }
+
+  public String getXzona() {
+    return xzona;
+  }
+
+  public void setXzona(String xzona) {
+    this.xzona = xzona;
+  }
+
+  public String getXbarrio() {
+    return xbarrio;
+  }
+
+  public void setXbarrio(String xbarrio) {
+    this.xbarrio = xbarrio;
+  }
+
+  public String getXtipoCalle() {
+    return xtipoCalle;
+  }
+
+  public void setXtipoCalle(String xtipoCalle) {
+    this.xtipoCalle = xtipoCalle;
+  }
+}
